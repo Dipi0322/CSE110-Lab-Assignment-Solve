776cb6281fdb3630150489ad0904b9cd09c50a19
Tasnoor Jahan Dipi <145858754+Dipi0322@users.noreply.github.com>
2023-12-30 04:41:58 +0600

    Delete Class_task_2.py

--- Class_task_2.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input(""))
-n2 = int(input(""))
-
-if n1 > n2:
-    print("First is greater")
-elif n1 < n2:
-    print("Second is greater")
-elif n1 == n2:
-    print("The numbers are equal")
